03d52afe63cee3c37104d3296a0524f6d4f2f774
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-03 16:27:17 +0300

    Introducing Components

--- section_06/components/app.js
+++ section_06/components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "cisu",
@@ -16,6 +15,29 @@ const app = Vue.createApp({
           email: "johndoe@email.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `<li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong> {{friend.phone}}</li>
+    <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+</li>
+`,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "cisu",
+        name: "Cisu Moeg",
+        phone: "6949999999",
+        email: "cisu@email.com",
+      },
     };
   },
   methods: {
