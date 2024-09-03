7c6aad1b946673b11a6a4fb4dad5c7184651b27c
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-03 14:17:23 +0300

    Vue Reactivity: A Deep Dive

--- section_05/behind-scenes/app.js
+++ section_05/behind-scenes/app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -15,4 +15,27 @@ const app = Vue.createApp({
   },
 });
 
-app.mount('#app');
+app.mount("#app");
+
+const data = {
+  message: "Hello!",
+  longMessage: "Hello! World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    // console.log(target);
+    // console.log(key);
+    // console.log(value);
+    if (key === "message") {
+      target.longMessage = value + " World";
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!!";
+
+console.log(proxy.longMessage);
