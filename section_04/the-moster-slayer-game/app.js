b1bc5cbaa809e534c1e06eef6163f4602075ce27
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-03 12:10:39 +0300

    Adding a "Special Attack"

--- section_04/the-moster-slayer-game/app.js
+++ section_04/the-moster-slayer-game/app.js
@@ -1,11 +1,13 @@
 const getRandomValue = (min, max) =>
-  Math.floor(Math.random() * (min - max)) + min;
+  Math.floor(Math.random() * (max - min + 1) + min);
+// Math.floor(Math.random() * (min - max)) + min;
 
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
 
@@ -16,19 +18,29 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
 
   methods: {
     attackMoster() {
-      //   const attachValue = getRandomValue(5, 12);
-      //   this.monsterHealth -= attachValue;
+      this.currentRound++;
+      // const attachValue = getRandomValue(5, 12);
+      // this.monsterHealth -= attachValue;
       this.monsterHealth -= getRandomValue(5, 12);
       this.attackPlayer();
     },
     attackPlayer() {
-      //   const attachValue = getRandomValue(8, 15);
-      //   this.playerHealth -= attachValue;
+      // const attachValue = getRandomValue(8, 15);
+      // this.playerHealth -= attachValue;
       this.playerHealth -= getRandomValue(8, 15);
+    },
+    specialAttackMoster() {
+      this.currentRound++;
+      const attachValue = getRandomValue(10, 25);
+      this.monsterHealth -= attachValue;
+      this.attackPlayer();
     },
   },
 });
