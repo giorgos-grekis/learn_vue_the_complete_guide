29031887f0fff4464cefc9a18e57a1cb3217bd25
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-03 11:41:53 +0300

    Project Setup & First Methods

---
+++ section_04/the-moster-slayer-game/app.js
@@ -0,0 +1,27 @@
+const getRandomValue = (min, max) =>
+  Math.floor(Math.random() * (min - max)) + min;
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+
+  methods: {
+    attackMoster() {
+      //   const attachValue = getRandomValue(5, 12);
+      //   this.monsterHealth -= attachValue;
+      this.monsterHealth -= getRandomValue(5, 12);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      //   const attachValue = getRandomValue(8, 15);
+      //   this.playerHealth -= attachValue;
+      this.playerHealth -= getRandomValue(8, 15);
+    },
+  },
+});
+
+app.mount("#game");
