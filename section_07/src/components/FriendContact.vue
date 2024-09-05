34bec0b04d6930d1fb51bc663b2866c9933b7a78
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-05 17:54:51 +0300

    Adding Styling

---
+++ section_07/src/components/FriendContact.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: { id: 'cisu', name: 'Cisu Moeg', phone: '6949999999', email: 'cis2@moeg.com' }
+    }
+  },
+
+  methods: {
+    toogleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
+
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
--- section_07/src/main.ts
+++ section_07/src/main.ts
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import FriendContact from './components/FriendContact.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.component('friend-contact', FriendContact)
+
+app.mount('#app')
