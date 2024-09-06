b85069c6c66c93fccfe74f6fabe44abaf55c8be2
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-06 17:39:07 +0300

    Moving to a Different Folder Structure

--- section_09/src/components/ManageGoals.vue
+++ section_09/src/components/ManageGoals.vue
@@ -27,19 +27,17 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
 
-    <teleport to="#dialog">
-      <error-alert v-if="inputIsInvalid">
-        <template #default>
-          <h2>Input is invalid</h2>
-          <p>Please enter at least a few characters...</p>
-          <button @click="confirmError">Okay</button>
-        </template>
-      </error-alert>
-    </teleport>
-  </div>
+  <teleport to="#dialog">
+    <error-alert v-if="inputIsInvalid">
+      <template #default>
+        <h2>Input is invalid</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </template>
+    </error-alert>
+  </teleport>
 </template>
