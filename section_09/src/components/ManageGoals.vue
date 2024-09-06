dbc37c6c074c3962b603575fbeee7d6867b7d9de
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-06 17:17:09 +0300

    Applying What We Know & A Problem

--- section_09/src/components/ManageGoals.vue
+++ section_09/src/components/ManageGoals.vue
@@ -1,6 +1,42 @@
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert: ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
+
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <template #default>
+        <h2>Input is invalid</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </template>
+    </error-alert>
   </div>
 </template>
