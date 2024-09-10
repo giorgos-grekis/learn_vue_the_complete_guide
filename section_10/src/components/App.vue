3660ba3a79a946b6d8fe616e4c55d9bf0eb57791
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-10 10:32:39 +0300

    Setup & First Steps

---
+++ section_10/src/components/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      /**
+       * @property {
+       *  Array<{
+       * id: string,
+       * title: string,
+       * description: string,
+       * link: string
+       * }>
+       * } storedResources
+       *
+       */
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org/'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://vuejs.org/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <ul></ul>
+</template>
