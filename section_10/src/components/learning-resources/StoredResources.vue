b1ec838493b88601f1a2745fd3e35b9e6c3911bb
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-10 11:28:12 +0300

    Styling and More Components

---
+++ section_10/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<script>
+import LearningResource from './LearningResource.vue'
+export default {
+  components: {
+    LearningResource
+  },
+  props: ['resources']
+}
+</script>
+
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    >
+    </learning-resource>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
