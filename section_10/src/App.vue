7d8c538963be7229bee26b9dca60f1fd080dc841
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-10 13:04:21 +0300

    Dynamic Components & Attribute Fallthrough

--- section_10/src/App.vue
+++ section_10/src/App.vue
@@ -1,10 +1,10 @@
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue'
+// import StoredResources from './components/learning-resources/StoredResources.vue'
 import TheHeader from './components/layouts/TheHeader.vue'
 
 export default {
   components: {
-    StoredResources,
+    // StoredResources,
     TheHeader
   },
   data() {
@@ -41,7 +41,7 @@ export default {
 
 <template>
   <the-header title="Remember Me"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <!-- <stored-resources :resources="storedResources"></stored-resources> -->
 </template>
 
 <style>
---
+++ section_10/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,27 @@
+<script>
+import StoredResources from './StoredResources.vue'
+import AddResource from './AddResource.vue'
+export default {
+  components: { StoredResources, AddResource },
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    }
+  },
+
+  method: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab
+    }
+  }
+}
+</script>
+
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resources')">Add Resources</base-button>
+  </base-card>
+
+  <component :is="selectedTab"> </component>
+</template>
