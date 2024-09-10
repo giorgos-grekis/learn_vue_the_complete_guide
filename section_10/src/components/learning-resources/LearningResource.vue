680489f3f46dcfab18fcc96796f1dd273a9098c2
giorgos-grekis <georgrekis.github@gmail.com>
2024-09-10 11:38:18 +0300

    Header & BaseCard Components

--- section_10/src/components/learning-resources/LearningResource.vue
+++ section_10/src/components/learning-resources/LearningResource.vue
@@ -6,16 +6,16 @@ export default {
 
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
